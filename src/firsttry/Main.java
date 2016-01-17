bcd5c1694d2c9c51e0ee6258c2eab2dc641dc0fc
BliznyukS <bliznyuk.sergey@gmail.com>
2016-01-17 16:16:57 +0200

    Test EPAM Method 1.0

---
+++ src/firsttry/Main.java
@@ -0,0 +1,8 @@
+package firsttry;
+
+public class Main {
+    public static void main (String[] args){
+        SloganAction firstObject = new SloganAction();
+        firstObject.printSlogan();
+    }
+}
---
+++ src/firsttry/SloganAction.java
@@ -0,0 +1,7 @@
+package firsttry;
+
+public class SloganAction {
+    void printSlogan(){
+        System.out.println("Enabling the information Age");
+    }
+}
