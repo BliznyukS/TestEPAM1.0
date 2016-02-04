dc71e4d09a56a4f9819525b45b9945961ad60205
BliznyukS <bliznyuk.sergey@gmail.com>
2016-02-04 22:48:32 +0200

    Test EPAM Method 1.0

---
+++ src/Siera/LittleX.java
@@ -0,0 +1,16 @@
+package Siera;
+
+/**
+ * Created by Yozhik on 04.02.2016.
+ */
+public class LittleX {
+    public static void main(String[] args) {
+        int x = 5;
+        while (x > 1) {
+            x = x - 1;
+            if (x < 3) {
+                System.out.println("Маленький Х");
+            }
+        }
+    }
+}
